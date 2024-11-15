0d2b4f42204fdab9296f31de643a821ae3cdb26e
RV <rauno.valk@voco.ee>
2024-11-15 17:21:23 +0200

    Ylesanne4

---
+++ Ylesanne4.js
@@ -0,0 +1,36 @@
+class Shape{
+    #color
+    constructor(color){
+        this.#color = color
+    }
+    setColor(color){
+        this.#color = color
+    } 
+    getColor(){
+        return this.#color
+    } 
+    getArea(){
+        return null
+    } 
+}
+
+class Circle extends Shape {
+    #radius
+    constructor(color, radius){
+        super(color)
+        this.#radius = radius
+    } 
+
+    getArea(){
+        return Math.PI* Math.pow(this.#radius, 2)
+    } 
+
+} 
+
+const shape1 = new Shape('red')
+console.log('shape color', shape1.getColor())
+console.log('shape area' , shape1.getArea())
+
+const circle1 = new Circle('blue', 5)
+console.log('circle color', circle1.getColor())
+console.log('circle area' , circle1.getArea())
